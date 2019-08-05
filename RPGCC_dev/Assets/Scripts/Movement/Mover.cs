9b993392630ba412c90b9d761e2d8736b3ed46ab
Eric <bookjunkie93@gmail.com>
2019-08-05 16:08:49 -0400

    Sandbox, Basic Movement, Follow Camera

---
+++ RPGCC_dev/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private NavMeshAgent m_rcAgent;
+    Animator m_rcAnimator;
+
+    Ray lastRay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rcAgent = GetComponent<NavMeshAgent>();
+        m_rcAnimator = GetComponent<Animator>();
+    }
+
+    public void OnUserClick ()
+    {
+              
+    }
+
+    void MoveToCursor ()
+    {
+        lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(lastRay, out hit);
+        if(hasHit)
+        {
+            m_rcAgent.SetDestination(hit.point);
+            
+        }
+        
+    }
+
+    void UpdateAnimator ()
+    {
+        Vector3 globalVelocity = m_rcAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(globalVelocity);
+        m_rcAnimator.SetFloat("forwardSpeed", localVelocity.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+           MoveToCursor();
+        }
+        UpdateAnimator();
+        Debug.DrawRay(lastRay.origin, lastRay.direction*100, Color.red);
+
+        
+    }
+}
